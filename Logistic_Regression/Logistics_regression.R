570069dc7c0ab78137939c7ea3526c57bc1f7a0a
preranavarsha770 <60514533+preranavarsha770@users.noreply.github.com>
2020-04-09 22:12:41 -0400

    Add files via upload

---
+++ Logistic_Regression/Logistics_regression.R
@@ -0,0 +1,61 @@
+who <- read.csv("C:/Users/prera/Downloads/Life_Expectancy_Data.csv")
+head(who)
+#############Dimesion of the dataset############################
+
+dim(who)
+
+##############     TOP 10 DEVELPED & DEVELOPING Countires      ######################
+
+status.of.countries <- who[(who$Status %in% c("Developing") & who$Life.expectancy<55) | (who$Status %in% c("Developed") & who$Life.expectancy>80) ,]
+dim(status.of.countries)
+View(status.of.countries)
+
+class(status.of.countries)
+head(status.of.countries)
+#View(status.of.countries)
+WHONew<-status.of.countries
+#resting the index values
+row.names(WHONew) <- NULL
+#View(WHONew)
+dim(WHONew)
+
+###################### CLEANING THE DATA ######################################
+# For 347 rows running the for loop for chechking any NA values and replacing it with the mean of the 
+# particular country.
+for(i in 1:347)
+{
+  if(is.na(WHONew$Alcohol[i]))
+  {
+    WHONew$Alcohol[i] <- with(WHONew, mean(WHONew$Alcohol[Country == WHONew$Country[i]], na.rm = TRUE))
+  }
+}
+for(i in 1:347)
+{
+  if(is.na(WHONew$Hepatitis.B[i]))
+  {
+    WHONew$Hepatitis.B[i] <- with(WHONew, mean(WHONew$Hepatitis.B[Country == WHONew$Country[i]], na.rm = TRUE))
+  }
+}
+for(i in 1:347)
+{
+  if(is.na(WHONew$Total.expenditure[i]))
+  {
+    WHONew$Total.expenditure[i] <- with(WHONew, mean(WHONew$Total.expenditure[Country == WHONew$Country[i]], na.rm = TRUE))
+  }
+}
+dim(WHONew)
+#View(WHONew)
+
+# Deleting the Empty rows where there is no data present.
+new.life<- na.omit(WHONew)
+dim(new.life)
+
+View(new.life)
+
+############ Finding the summary or the distribution of the Dataset ##################
+str(new.life)
+
+################# USing the Logistic Regression ###################
+
+logistic_simple <- glm(Life.expectancy ~ ., data=new.life, family="binomial")
+summary(logistic_simple)
